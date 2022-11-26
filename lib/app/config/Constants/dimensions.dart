0da7a1ed607469dbaac47863418131c5d42b9002
MostafaKhalile <mostafa.khalile.aboheaba@gmail.com>
2022-11-26 10:40:58 +0200

    welcome Container

---
+++ lib/app/config/Constants/dimensions.dart
@@ -0,0 +1,3 @@
+const ksmallMargin = 8.0;
+const kMediumMargin = 16.0;
+const kLargeMargin = 32.0;
--- lib/app/modules/menu/views/menu_view.dart
+++ lib/app/modules/menu/views/menu_view.dart
@@ -1,25 +1,46 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:school_every_where_v2/app/config/Constants/dimensions.dart';
 import 'package:school_every_where_v2/app/config/theme/theme.dart';
 
 import '../../../widgets/custom_drawer.dart';
+import '../../../widgets/day_night_greating.dart';
 import '../controllers/menu_controller.dart';
 
 class MenuView extends GetView<MenuController> {
   const MenuView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: AppColors.scaffoldBackgroundColor,
-        iconTheme: const IconThemeData(color: AppColors.black),
-      ),
-      drawer: const CustomDrawer(),
-      body: const Center(
-        child: Text(
-          'MenuView is working',
-          style: TextStyle(fontSize: 20),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          
+          backgroundColor: AppColors.scaffoldBackgroundColor,
+          iconTheme: const IconThemeData(color: AppColors.black),
+          actions: [
+            IconButton(
+                splashRadius: 15,
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.notifications_active,
+                  size: 25,
+                ))
+          ],
+        ),
+        drawer: const CustomDrawer(),
+        body: SingleChildScrollView(
+          child: Column(
+            children: const [
+              DayNightGreetingBanner(),
+              Center(
+                child: Text(
+                  'MenuView is working',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
