c5d8dfbd772bdbc45b2de100b658a81fc72acb4f
MostafaKhalile <mostafa.khalile.aboheaba@gmail.com>
2022-12-08 15:10:47 +0200

    network sensetivity for home and login

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,10 +1,10 @@
 import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-import '../../../widgets/custom_drawer.dart';
 
 import '../../../config/theme/theme.dart';
+import '../../../widgets/custom_drawer.dart';
+import '../../Connectivity/views/network_sensitive.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
@@ -12,85 +12,88 @@ class HomeView extends GetView<HomeController> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: const CustomDrawer(),
-      floatingActionButton: FloatingActionButton(
-          backgroundColor: AppColors.redDark,
-          onPressed: () => controller.logOut(),
-          child: const Icon(
-            Icons.power_settings_new,
-            color: AppColors.white,
-          )),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      body: Obx(() => controller.screens[controller.index.value]),
-      bottomNavigationBar: Obx(
-        () => BottomAppBar(
-          shape: const CircularNotchedRectangle(),
-          child:
-              Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: IconButton(
-                icon: Icon(
-                  Icons.maps_home_work_rounded,
-                  color: (controller.index.value == 0)
-                      ? AppColors.primaryColor
-                      : AppColors.secondaryTypoColor,
-                ),
-                onPressed: () => controller.changeIndex(0),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: IconButton(
-                icon: Icon(
-                  Icons.chat,
-                  color: controller.index.value == 1
-                      ? AppColors.primaryColor
-                      : AppColors.secondaryTypoColor,
-                ),
-                onPressed: () => controller.changeIndex(1),
-              ),
-            ),
-            SizedBox(
-              width: context.width * 0.2,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: IconButton(
-                icon: Badge(
-                  animationType: BadgeAnimationType.slide,
-                  badgeContent: Padding(
-                    padding: const EdgeInsets.all(1.0),
-                    child: Text(
-                      "3",
-                      style: context.textTheme.button!
-                          .copyWith(color: AppColors.white),
+    return NetworkSensitive(
+      child: Scaffold(
+        drawer: const CustomDrawer(),
+        floatingActionButton: FloatingActionButton(
+            backgroundColor: AppColors.redDark,
+            onPressed: () => controller.logOut(),
+            child: const Icon(
+              Icons.power_settings_new,
+              color: AppColors.white,
+            )),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        body: Obx(() => controller.screens[controller.index.value]),
+        bottomNavigationBar: Obx(
+          () => BottomAppBar(
+            shape: const CircularNotchedRectangle(),
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: IconButton(
+                      icon: Icon(
+                        Icons.maps_home_work_rounded,
+                        color: (controller.index.value == 0)
+                            ? AppColors.primaryColor
+                            : AppColors.secondaryTypoColor,
+                      ),
+                      onPressed: () => controller.changeIndex(0),
                     ),
                   ),
-                  child: Icon(
-                    Icons.notifications,
-                    color: controller.index.value == 2
-                        ? AppColors.primaryColor
-                        : AppColors.secondaryTypoColor,
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: IconButton(
+                      icon: Icon(
+                        Icons.chat,
+                        color: controller.index.value == 1
+                            ? AppColors.primaryColor
+                            : AppColors.secondaryTypoColor,
+                      ),
+                      onPressed: () => controller.changeIndex(1),
+                    ),
                   ),
-                ),
-                onPressed: () => controller.changeIndex(2),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: IconButton(
-                icon: Icon(
-                  Icons.person,
-                  color: controller.index.value == 3
-                      ? AppColors.primaryColor
-                      : AppColors.secondaryTypoColor,
-                ),
-                onPressed: () => controller.changeIndex(3),
-              ),
-            ),
-          ]),
+                  SizedBox(
+                    width: context.width * 0.2,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: IconButton(
+                      icon: Badge(
+                        animationType: BadgeAnimationType.slide,
+                        badgeContent: Padding(
+                          padding: const EdgeInsets.all(1.0),
+                          child: Text(
+                            "3",
+                            style: context.textTheme.button!
+                                .copyWith(color: AppColors.white),
+                          ),
+                        ),
+                        child: Icon(
+                          Icons.notifications,
+                          color: controller.index.value == 2
+                              ? AppColors.primaryColor
+                              : AppColors.secondaryTypoColor,
+                        ),
+                      ),
+                      onPressed: () => controller.changeIndex(2),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: IconButton(
+                      icon: Icon(
+                        Icons.person,
+                        color: controller.index.value == 3
+                            ? AppColors.primaryColor
+                            : AppColors.secondaryTypoColor,
+                      ),
+                      onPressed: () => controller.changeIndex(3),
+                    ),
+                  ),
+                ]),
+          ),
         ),
       ),
     );
--- lib/app/widgets/scaffold_oval_clipper.dart
+++ lib/app/widgets/scaffold_oval_clipper.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../config/utils/flavor_config.dart';
+import '../modules/Connectivity/views/network_sensitive.dart';
 import 'oval_bottom_clipper.dart';
 
 class ScaffoldOvalClipper extends StatelessWidget {
@@ -10,58 +11,60 @@ class ScaffoldOvalClipper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Stack(alignment: Alignment.topCenter, children: [
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: SizedBox(
-              height: context.height * 0.55,
-              child: NotificationListener<OverscrollIndicatorNotification>(
-                  onNotification: (overScroll) {
-                    overScroll.disallowIndicator();
-                    return true;
-                  },
-                  child: Align(
-                      alignment: Alignment.bottomCenter,
-                      child: SingleChildScrollView(child: child))),
+    return NetworkSensitive(
+      child: SafeArea(
+        child: Scaffold(
+          body: Stack(alignment: Alignment.topCenter, children: [
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: SizedBox(
+                height: context.height * 0.55,
+                child: NotificationListener<OverscrollIndicatorNotification>(
+                    onNotification: (overScroll) {
+                      overScroll.disallowIndicator();
+                      return true;
+                    },
+                    child: Align(
+                        alignment: Alignment.bottomCenter,
+                        child: SingleChildScrollView(child: child))),
+              ),
             ),
-          ),
-          ClipPath(
-            clipper: OvalBottomBorderClipper(),
-            child: Container(
-                width: context.width,
-                height: context.height * 0.4,
-                decoration: const BoxDecoration(
-                  // color: Colors.grey[500],
-                  color: Colors.white30,
-                  // backgroundBlendMode: BlendMode.overlay,
-                  image: DecorationImage(
-                      // image: AssetImage("assets/pattern_white.png"),
-                      image: AssetImage("assets/pattern.png"),
-                      fit: BoxFit.cover),
-                )),
-          ),
-          Positioned(
-            top: context.height * 0.30,
-            right: 0,
-            left: 0,
-            child: CircleAvatar(
-              backgroundColor: Colors.white,
-              radius: 70,
-              child: Center(
-                child: SizedBox(
-                  height: 120,
-                  width: 120,
-                  child: Image.asset(
-                    FlavorConfig.instance.values.imagePath!,
-                    fit: BoxFit.contain,
+            ClipPath(
+              clipper: OvalBottomBorderClipper(),
+              child: Container(
+                  width: context.width,
+                  height: context.height * 0.4,
+                  decoration: const BoxDecoration(
+                    // color: Colors.grey[500],
+                    color: Colors.white30,
+                    // backgroundBlendMode: BlendMode.overlay,
+                    image: DecorationImage(
+                        // image: AssetImage("assets/pattern_white.png"),
+                        image: AssetImage("assets/pattern.png"),
+                        fit: BoxFit.cover),
+                  )),
+            ),
+            Positioned(
+              top: context.height * 0.30,
+              right: 0,
+              left: 0,
+              child: CircleAvatar(
+                backgroundColor: Colors.white,
+                radius: 70,
+                child: Center(
+                  child: SizedBox(
+                    height: 120,
+                    width: 120,
+                    child: Image.asset(
+                      FlavorConfig.instance.values.imagePath!,
+                      fit: BoxFit.contain,
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-        ]),
+          ]),
+        ),
       ),
     );
   }
