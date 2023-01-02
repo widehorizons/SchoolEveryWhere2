502c0bbc07448638446a5d48be4e0ade9182deb4
MostafaKhalile <mostafa.khalile.aboheaba@gmail.com>
2023-01-02 08:19:55 +0200

    teddy login screen

--- lib/app/modules/Login/controllers/login_controller.dart
+++ lib/app/modules/Login/controllers/login_controller.dart
@@ -1,12 +1,98 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:rive/rive.dart';
 
 import '../../../config/Constants/prefs_keys.dart';
 import '../../../config/utils/prefs.dart';
 import '../../../routes/app_pages.dart';
 
 class LoginController extends GetxController {
+  var dropdownvalue;
+  var dropdownvalueSubject;
+  var subjectItemlist = [];
+
+  List userTypeItemlist = ['Student', 'Parent', 'Staff', 'Manager'];
+
+  late String animationURL;
+  Rx<Artboard?> teddyArtboard = Rx<Artboard?>(null);
+  SMITrigger? successTrigger, failTrigger;
+  SMIBool? isHandsUp, isChecking;
+  SMINumber? numLook;
+
+  StateMachineController? stateMachineController;
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
   void login() {
-    Prefs.setString(PrefsKeys.token, "token");
-    Get.offAllNamed(Routes.HOME);
+    isChecking?.change(false);
+    isHandsUp?.change(false);
+    if (emailController.text == "admin" && passwordController.text == "admin") {
+      successTrigger?.fire();
+      Future.delayed(
+          const Duration(seconds: 2),
+          () => {
+                Prefs.setString(PrefsKeys.token, "token"),
+                Get.offAllNamed(Routes.HOME)
+              });
+    } else {
+      failTrigger?.fire();
+    }
+  }
+
+  void handsOnTheEyes() {
+    isHandsUp?.change(true);
+  }
+
+  void lookOnTheTextField() {
+    isHandsUp?.change(false);
+    isChecking?.change(true);
+    numLook?.change(0);
+  }
+
+  void moveEyeBalls(val) {
+    numLook?.change(val.length.toDouble());
+  }
+
+  void doNoThing() {
+    isHandsUp?.change(false);
+    isChecking?.change(false);
+    numLook?.change(0);
+  }
+
+  @override
+  void onInit() {
+    rootBundle.load('assets/riveAssets/login.riv').then((data) {
+      final file = RiveFile.import(data);
+      final artboard = file.mainArtboard;
+      stateMachineController =
+          StateMachineController.fromArtboard(artboard, "Login Machine");
+      if (stateMachineController != null) {
+        artboard.addController(stateMachineController!);
+
+        for (var e in stateMachineController!.inputs) {
+          log(e.runtimeType.toString());
+          log("name${e.name}End");
+        }
+
+        for (var element in stateMachineController!.inputs) {
+          if (element.name == "trigSuccess") {
+            successTrigger = element as SMITrigger;
+          } else if (element.name == "trigFail") {
+            failTrigger = element as SMITrigger;
+          } else if (element.name == "isHandsUp") {
+            isHandsUp = element as SMIBool;
+          } else if (element.name == "isChecking") {
+            isChecking = element as SMIBool;
+          } else if (element.name == "numLook") {
+            numLook = element as SMINumber;
+          }
+        }
+      }
+      teddyArtboard.value = artboard;
+      update();
+    });
+    super.onInit();
   }
 }
--- lib/app/modules/Login/views/login_view.dart
+++ lib/app/modules/Login/views/login_view.dart
@@ -1,28 +1,150 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:rive/rive.dart';
 
 import '../../../widgets/custom_button.dart';
-import '../../../widgets/scaffold_oval_clipper.dart';
 import '../controllers/login_controller.dart';
 
 class LoginView extends GetView<LoginController> {
   const LoginView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return ScaffoldOvalClipper(
-      child: SizedBox(
-        height: context.height,
-        child: Center(
-          child: SingleChildScrollView(
-            child: Column(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  CustomButton(
-                      onPressed: controller.login, child: Text("login".tr)),
-                ]),
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: const Color(0xffd6e2ea),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Obx(
+            () => (controller.teddyArtboard.value != null)
+                ? SizedBox(
+                    width: context.width * 0.9,
+                    height: context.height * 0.33,
+                    child: Rive(
+                      artboard: controller.teddyArtboard.value!,
+                      alignment: Alignment.bottomCenter,
+                      fit: BoxFit.contain,
+                    ),
+                  )
+                : const SizedBox(),
           ),
-        ),
+          Container(
+            width: context.width * .9,
+            padding: const EdgeInsets.only(bottom: 15),
+            margin: const EdgeInsets.only(bottom: 15 * 4),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 15 * 2),
+                      DropdownButton(
+                        hint: const Text('Select a user type'),
+                        elevation: 4,
+                        isDense: true,
+                        // icon: const Icon(Icons.arrow_circle_down_sharp),
+                        items: controller.userTypeItemlist.map((item) {
+                          return DropdownMenuItem(
+                            value: item,
+                            child: Text(item),
+                          );
+                        }).toList(),
+                        onTap: controller.doNoThing,
+                        onChanged: (newVal) {
+                          controller.dropdownvalue = newVal;
+                        },
+                        value: controller.dropdownvalue,
+                      ),
+                      const SizedBox(height: 15),
+                      TextField(
+                        controller: controller.emailController,
+                        onTap: controller.lookOnTheTextField,
+                        onChanged: controller.moveEyeBalls,
+                        keyboardType: TextInputType.emailAddress,
+                        style: const TextStyle(fontSize: 14),
+                        cursorColor: const Color(0xffb04863),
+                        decoration: const InputDecoration(
+                          hintText: "Email/Username",
+                          filled: true,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                          ),
+                          focusColor: Color(0xffb04863),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Color(0xffb04863),
+                            ),
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 15),
+                      TextField(
+                        controller: controller.passwordController,
+                        onTap: controller.handsOnTheEyes,
+                        keyboardType: TextInputType.visiblePassword,
+                        obscureText: true,
+                        style: const TextStyle(fontSize: 14),
+                        cursorColor: const Color(0xffb04863),
+                        decoration: const InputDecoration(
+                          hintText: "Password",
+                          filled: true,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                          ),
+                          focusColor: Color(0xffb04863),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Color(0xffb04863),
+                            ),
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 15),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          //remember me checkbox
+                          Row(
+                            children: [
+                              Checkbox(
+                                value: false,
+                                onChanged: (value) {},
+                              ),
+                              const Text("Remember me"),
+                            ],
+                          ),
+                          CustomButton(
+                            onPressed: controller.login,
+                            child: const Text("Login"),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Image.asset(
+            'img/schooleverywhere.png',
+            width: 50,
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Text(
+              '© Powerd by SchoolEveryWhere™',
+              style: context.textTheme.bodyMedium!.copyWith(height: 1.2),
+            ),
+          ),
+        ],
       ),
     );
   }
